26d27dc0ba1738e7c8f60664df65f2939656a290
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-08 14:55:02 +0200

    Create main.adb

---
+++ main.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+begin
+   Put_Line ("Cheney's Stop-and-Copy Garbage Collector.");
+   Put_Line ("Run 'make test' to execute the V&V verification suite.");
+end Main;
